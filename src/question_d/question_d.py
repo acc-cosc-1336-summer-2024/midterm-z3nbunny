30464831ad4f185dabb2b23cdc62eea814a0978e
z3nbunny <starfire3d@gmail.com>
2024-06-27 05:58:00 +0000

    Question D all tests ok

--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,9 @@
 #write functions here, don't add input('') statements here!
+
+def reverse_string(string1):
+    reversed_string = ""
+    i = len(string1) - 1
+    while i >= 0:
+        reversed_string += string1[i]
+        i -= 1
+    return reversed_string
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,6 +2,7 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
+from src.question_d.question_d import reverse_string
 from src.question_c.question_c import get_miles_per_hour
 from src.question_b.question_b import get_fahrenheit
 from src.question_a.question_a import get_assessment_value, get_tax_assessed, test_config
@@ -27,3 +28,7 @@ class Test_Config(unittest.TestCase):
 
     def test_get_miles_per_hour(self):
         self.assertEqual(19.883872, get_miles_per_hour(32, 60))
+
+    def test_reverse_string(self):
+        self.assertEqual("dlrow olleh", reverse_string("hello world"))
+        self.assertEqual("olleh", reverse_string("hello"))
